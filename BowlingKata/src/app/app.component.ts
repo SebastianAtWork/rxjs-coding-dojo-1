b911c134f1465e11927a08ea1f3373f4e10fa03c
Sebastian Schwarz <Sebastian.Schwarz@generic.de>
2018-10-12 13:40:32 +0200

    subscribe to Observable

--- BowlingKata/src/app/app.component.ts
+++ BowlingKata/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { BowlingInputService } from './bowling-input.service';
 
@@ -7,13 +8,22 @@ import { BowlingInputService } from './bowling-input.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+
   title = 'app';
+  _subscription: Subscription;
   constructor(private bowlingInputService: BowlingInputService) {
 
+    this._subscription = this.bowlingInputService.Throws.subscribe(pinCount => this.onThrow(pinCount));
   }
 
   public NextThrow(pinCount: string) {
     this.bowlingInputService.NextThrow(+pinCount);
+  }
+
+  public onThrow(pinCount: number) {
+  }
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
   }
 }
